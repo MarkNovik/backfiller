8d0bb2b7655530502d43de95b5e4273397b09b4d
MarkNovik <novikmark@gmail.com>
2024-01-10 20:26:43 +0200

    Improved UI

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,15 +8,16 @@ import androidx.compose.material.Button
 import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toComposeImageBitmap
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import com.darkrockstudios.libraries.mpfilepicker.DirectoryPicker
 import com.darkrockstudios.libraries.mpfilepicker.MultipleFilePicker
@@ -30,9 +31,7 @@ import kotlin.random.nextInt
 const val STEP_MULTIPLIER = 20
 
 enum class Picking {
-    None,
-    File,
-    Dir
+    None, File, Dir
 }
 
 fun generateRandomImage(
@@ -41,28 +40,23 @@ fun generateRandomImage(
     h: Int,
     size: Float,
     density: Float,
-): BufferedImage =
-    BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB).apply {
-        val stepX = (1 / density).toInt() * STEP_MULTIPLIER
-        val stepY = (1 / density).toInt() * STEP_MULTIPLIER
-        for (x in 0..<w step stepX) {
-            for (y in 0..<h step stepY) {
-                val nx = x + Random.nextInt((-stepX / 2)..(stepX / 2))
-                val ny = y + Random.nextInt((-stepY / 2)..(stepY / 2))
-                //if (Random.nextDouble() < density)
-                fill.randomOrNull()?.let { img ->
-                    draw(img, nx, ny, (img.width * size).toInt(), (img.height * size).toInt(), .5, .5)
-                }
+): BufferedImage = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB).apply {
+    val stepX = (1 / density).toInt() * STEP_MULTIPLIER
+    val stepY = (1 / density).toInt() * STEP_MULTIPLIER
+    for (x in 0..<w step stepX) {
+        for (y in 0..<h step stepY) {
+            val nx = x + Random.nextInt((-stepX / 2)..(stepX / 2))
+            val ny = y + Random.nextInt((-stepY / 2)..(stepY / 2))
+            //if (Random.nextDouble() < density)
+            fill.randomOrNull()?.let { img ->
+                draw(img, nx, ny, (img.width * size).toInt(), (img.height * size).toInt(), .5, .5)
             }
         }
     }
+}
 
 fun BufferedImage.draw(
-    img: BufferedImage,
-    startX: Int, startY: Int,
-    w: Int, h: Int,
-    anchorX: Double = 0.0,
-    anchorY: Double = 0.0
+    img: BufferedImage, startX: Int, startY: Int, w: Int, h: Int, anchorX: Double = 0.0, anchorY: Double = 0.0
 ) {
     if (w == 0 || h == 0) return
     if (w < 0 || h < 0) error("Negative w or h are unsupported")
@@ -92,14 +86,14 @@ fun main() = application {
     var path by remember { mutableStateOf("C:\\") }
     var outDir by remember { mutableStateOf("") }
     var outName by remember { mutableStateOf("generated.png") }
-    var readImgs: List<BufferedImage> by remember { mutableStateOf(emptyList()) }
-    var sizeMul by remember { mutableStateOf(1f) }
-    var density by remember { mutableStateOf(.5f) }
+    val readImgs = remember { mutableStateListOf<BufferedImage>() }
+    var sizeMul by remember { mutableFloatStateOf(1f) }
+    var density by remember { mutableFloatStateOf(.5f) }
     var fileReading: Thread? by remember { mutableStateOf(null) }
     var generating: Thread? by remember { mutableStateOf(null) }
     var saving: Thread? by remember { mutableStateOf(null) }
-    var resW by remember { mutableStateOf(1000) }
-    var resH by remember { mutableStateOf(1000) }
+    var resW by remember { mutableIntStateOf(1000) }
+    var resH by remember { mutableIntStateOf(1000) }
     var status by remember { mutableStateOf("Idle") }
     var res by remember {
         mutableStateOf(BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB))
@@ -120,9 +114,10 @@ fun main() = application {
             path = files.joinToString(", ") { File(it.path).name }
             fileReading?.interrupt()
             fileReading = thread {
-                readImgs = files.mapNotNull { file ->
+                readImgs.clear()
+                readImgs.addAll(files.mapNotNull { file ->
                     runOrNull { ImageIO.read(File(file.path)) }
-                }
+                })
             }
         }
         picking = Picking.None
@@ -132,8 +127,9 @@ fun main() = application {
             path = it
             fileReading?.interrupt()
             fileReading = thread {
-                readImgs = File(path).listFiles()?.filterNotNull()?.filter(File::isFile)
-                    ?.mapNotNull { runOrNull { ImageIO.read(it) } } ?: emptyList()
+                readImgs.clear()
+                readImgs.addAll(File(path).listFiles()?.filterNotNull()?.filter(File::isFile)
+                    ?.mapNotNull { runOrNull { ImageIO.read(it) } } ?: emptyList())
             }
         }
         picking = Picking.None
@@ -142,67 +138,72 @@ fun main() = application {
         p?.let { outDir = it }
         pickingOut = false
     }
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        title = "BackFiller", onCloseRequest = ::exitApplication, state = WindowState(
+            size = DpSize(1200.dp, 600.dp)
+        )
+    ) {
         Row {
             Column(
                 modifier = Modifier.weight(1f)
             ) {
-                TextField(
-                    path,
+                TextField(path,
                     {}, readOnly = true,
                     modifier = Modifier.fillMaxWidth(),
-                    label = { Text("Input image(s)") }
-                )
+                    label = { Text("Input image(s)") })
                 Row {
-                    Button({ picking = Picking.File }) { Text("Select file(s)") }
-                    Button({ picking = Picking.Dir }) { Text("Select a directory") }
+                    Button({ picking = Picking.File }, modifier = Modifier.weight(1f)) { Text("Select file(s)") }
+                    Button({ picking = Picking.Dir }, modifier = Modifier.weight(1f)) { Text("Select a directory") }
                 }
                 Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-                    Text("Size", modifier = Modifier.weight(1f))
+                    Text("Size", modifier = Modifier.weight(1f), textAlign = TextAlign.Center)
                     Slider(sizeMul, { sizeMul = it }, valueRange = 0f..2f, modifier = Modifier.weight(5f))
-                    Text(sizeMul.toString(), Modifier.weight(1f), maxLines = 1)
+                    Text(sizeMul.toString(), Modifier.weight(.5f), maxLines = 1, textAlign = TextAlign.Center)
                 }
                 Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-                    Text("Density", modifier = Modifier.weight(1f))
+                    Text("Density", modifier = Modifier.weight(1f), textAlign = TextAlign.Center)
                     Slider(density, { density = it }, valueRange = .1f..0.9f, modifier = Modifier.weight(5f))
-                    Text(density.toString(), Modifier.weight(1f), maxLines = 1)
+                    Text(density.toString(), Modifier.weight(.5f), maxLines = 1, textAlign = TextAlign.Center)
                 }
                 Row {
-                    TextField(
-                        resW.toString(),
+                    TextField(resW.toString(),
                         { resW = it.filter(Char::isDigit).toIntOrNull() ?: resW },
                         modifier = Modifier.weight(1f),
-                        label = { Text("Image width") }
-                    )
-                    TextField(
-                        resH.toString(),
+                        label = { Text("Image width") })
+                    TextField(resH.toString(),
                         { resH = it.filter(Char::isDigit).toIntOrNull() ?: resH },
                         modifier = Modifier.weight(1f),
-                        label = { Text("Image height") }
-                    )
+                        label = { Text("Image height") })
                 }
                 Row(verticalAlignment = Alignment.CenterVertically) {
-                    Button({ regen() }) {
+                    Button({ regen() }, modifier = Modifier.weight(1f)) {
                         Text("Generate")
                     }
-                    Button({ generating?.interrupt(); status = "Cancelled" }) {
+                    Text("Status: $status", modifier = Modifier.weight(.5f), textAlign = TextAlign.Center)
+                    Button({ generating?.interrupt(); status = "Cancelled" }, modifier = Modifier.weight(1f)) {
                         Text("Cancel")
                     }
-                    Text("Status: $status")
                 }
                 Column {
                     Row {
-                        TextField(outDir, {}, readOnly = true,
-                            label = { Text("Output directory") }
+                        TextField(
+                            outDir,
+                            {}, readOnly = true,
+                            label = { Text("Output directory") },
+                            modifier = Modifier.weight(1f)
                         )
                         TextField(
-                            outName, { outName = it },
+                            outName,
+                            { outName = it },
                             label = { Text("Generated image name") },
+                            modifier = Modifier.weight(1f)
                         )
                     }
                     Row {
-                        Button({ pickingOut = true }) { Text("Select output directory") }
-                        Button({
+                        Button(
+                            { pickingOut = true }, modifier = Modifier.weight(1f)
+                        ) { Text("Select output directory") }
+                        Button(modifier = Modifier.weight(1f), onClick = {
                             val file = File("$outDir/$outName")
                             file.mkdirs()
                             file.createNewFile()
@@ -219,12 +220,12 @@ fun main() = application {
                 }
             }
             Column(
-                modifier = Modifier.weight(1f).background(Color.Gray)
+                modifier = Modifier.weight(1f)
             ) {
                 Image(
                     res.toComposeImageBitmap(),
                     "Generated Image",
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize().background(Color.LightGray)
                 )
             }
         }
